2567bec3422ca4d7098bfbc1f81ea01817bce4e4
Joonas Villanen <joonas.villa@gmail.com>
2022-01-27 19:03:36 +0100

    added gif component

---
+++ src/components/gif.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Gif extends Component {
+  render() {
+    return (
+      <img src="https://media3.giphy.com/media/l3fQf1OEAq0iri9RC/200w.gif" alt="" className='gif' />
+    );
+  }
+}
+
+export default Gif;
